6a0164405f49e423b59d7d2306d25d21c08040ce
Asem Abdelsalam <assemmohamed1000@gmail.com>
2025-04-03 18:21:22 +0200

    Remove css js

--- Js/script.js
+++
@@ -1,7 +0,0 @@
-import { initNavigation } from './navigation.js';
-import { initInteractivity } from './interactivity.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initNavigation();
-    initInteractivity();
-});
