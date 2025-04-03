f6ae8cb02a86382ff40bb0d2eef67f7a8f5f3ebe
Asem Abdelsalam <assemmohamed1000@gmail.com>
2025-04-03 17:43:56 +0200

    Initial commit

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+import { initNavigation } from './navigation.js';
+import { initInteractivity } from './interactivity.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initNavigation();
+    initInteractivity();
+});
